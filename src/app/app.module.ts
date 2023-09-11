31aef2117c343a49433310cc928771f103af12a0
AGmaj <adrian.gmaj@gmail.com>
2023-09-11 19:31:25 +0200

    Flowers v.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { HomeModule } from './home/home.module';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing-module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HomeModule,
+    RouterModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
